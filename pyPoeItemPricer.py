e117fb8e28ab892d4a2908bb040a14128220a74b
mort <mortimermcmiresa@gmail.com>
2019-06-18 01:24:35 -0400

    gg ez done

--- pyPoeItemPricer.py
+++ pyPoeItemPricer.py
@@ -41,94 +41,101 @@ class App(threading.Thread):
         previous_item = 0
         acc = ['Ring', 'Amulet', 'Belt', 'Talisman']
         while True:
-            keyboard.wait('ctrl+c')
-            ROOT.lift()
-            rawitem = pyperclip.paste()
-            price = 0.0
-            if re.search(r'\r\n',rawitem):
-                splititem = rawitem.split('\r\n')
-                item_type = splititem[2]
+            if keyboard.is_pressed('ctrl+c'):
+                windowloc = "+" + str(ROOT.winfo_pointerx()) + "+" + str(ROOT.winfo_pointery())
+                ROOT.geometry(windowloc)
+                ROOT.attributes("-alpha", 1)
+                rawitem = pyperclip.paste()
+                price = 0.0
+                if re.search(r'\r\n',rawitem):
+                    splititem = rawitem.split('\r\n')
+                    item_type = splititem[2]
 
-                itemrarity = splititem[0].split(':')[1][1:]
+                    itemrarity = splititem[0].split(':')[1][1:]
 
-                if itemrarity == 'Divination Card':
-                    itemname = splititem[1]
+                    if itemrarity == 'Divination Card':
+                        itemname = splititem[1]
 
-                    if itemname == previous_item:
-                        continue
-                    
-                    previous_item = itemname
-
-                    namefilter = [s for s in card_json_parsed if itemname in s['name']]
-                    price = namefilter[0]['mean']
-
-                if 'Right-click to add this prophecy' in splititem[-2]: #lol
-                    itemname = splititem[1]
-                    if itemname == previous_item:
-                        continue
-                    
-                    previous_item = itemname
-
-                    namefilter = [s for s in prophecy_json_parsed if itemname in s['name']]
-                    price = namefilter[0]['mean']
-
-                if itemrarity == 'Unique':
-                    itemname = splititem[1]
-                    
-                    if re.match("<<", itemname): #wtf is this
-                        itemname = itemname.split(">>")[3]
-
-                    if itemname == previous_item:
-                        continue
-
-                    previous_item = itemname
-                    socketraw = [s for s in splititem if "Sockets" in s]
-                    
-                    if socketraw:
-                        socketcount = len(re.findall('-', socketraw[0]))+1
+                        if itemname == previous_item:
+                            continue
                         
-                        if socketcount < 5:
-                            socketcount = None
-                    else:
-                        socketcount = None
+                        previous_item = itemname
 
-                    if [k for k in acc if k in item_type]:
-                        namefilter = [s for s in accessories_json_parsed if itemname in s['name']]
-                    elif 'Jewel' in item_type:
-                        namefilter = [s for s in jewel_json_parsed if itemname in s['name']]
-                    elif 'Flask' in item_type:
-                        namefilter = [s for s in flask_json_parsed if itemname in s['name']]
-                    elif 'Map' in item_type:
-                        namefilter = [s for s in map_json_parsed if itemname in s['name']]
-                    else:
-                        namefilter = [s for s in weapon_json_parsed if itemname in s['name']]
-                        if len(namefilter) == 0:
-                            namefilter = [s for s in armor_json_parsed if itemname in s['name']]
+                        namefilter = [s for s in card_json_parsed if itemname in s['name']]
+                        price = namefilter[0]['mean']
 
+                    if 'Right-click to add this prophecy' in splititem[-2]: #lol
+                        itemname = splititem[1]
+                        if itemname == previous_item:
+                            continue
+                        
+                        previous_item = itemname
 
-                    sockfilter = []
-                    for k in namefilter:
-                        if 'linkCount' in k:                            
-                            if socketcount == k['linkCount']:
-                                  sockfilter = k
+                        namefilter = [s for s in prophecy_json_parsed if itemname in s['name']]
+                        price = namefilter[0]['mean']
+
+                    if itemrarity == 'Unique':
+                        itemname = splititem[1]
+                        
+                        if re.match("<<", itemname): #wtf is this
+                            itemname = itemname.split(">>")[3]
+
+                        if itemname == previous_item:
+                            continue
+
+                        previous_item = itemname
+                        socketraw = [s for s in splititem if "Sockets" in s]
+                        
+                        if socketraw:
+                            socketcount = len(re.findall('-', socketraw[0]))+1
+                            
+                            if socketcount < 5:
+                                socketcount = None
                         else:
-                            k['linkCount'] = None
-                            if socketcount == k['linkCount']:
-                                  sockfilter = k
+                            socketcount = None
 
-                    if len(sockfilter) == 0:
-                        price = "Not enough data for 6 link of this item available."
-                    else:
-                        price = sockfilter['mean']                       
+                        if [k for k in acc if k in item_type]:
+                            namefilter = [s for s in accessories_json_parsed if itemname in s['name']]
+                        elif 'Jewel' in item_type:
+                            namefilter = [s for s in jewel_json_parsed if itemname in s['name']]
+                        elif 'Flask' in item_type:
+                            namefilter = [s for s in flask_json_parsed if itemname in s['name']]
+                        elif 'Map' in item_type:
+                            namefilter = [s for s in map_json_parsed if itemname in s['name']]
+                        else:
+                            namefilter = [s for s in weapon_json_parsed if itemname in s['name']]
+                            if len(namefilter) == 0:
+                                namefilter = [s for s in armor_json_parsed if itemname in s['name']]
 
-                        
-            if price != 0.0:
-                prettyprice = str(price)
-                print(f'{itemname}: {prettyprice[0:4]}c')
+
+                        sockfilter = []
+                        for k in namefilter:
+                            if 'linkCount' in k:                            
+                                if socketcount == k['linkCount']:
+                                      sockfilter = k
+                            else:
+                                k['linkCount'] = None
+                                if socketcount == k['linkCount']:
+                                      sockfilter = k
+
+                        if len(sockfilter) == 0:
+                            price = "Not enough data for 6 link of this item available."
+                        else:
+                            price = sockfilter['mean']                       
+
+                            
+                if price != 0.0:
+                    prettyprice = str(price)
+                    print(f'{itemname}: {prettyprice[0:4]}c')
+                    LABEL.config(text=f'{itemname}: {prettyprice[0:4]}c')
+            else:
+                ROOT.attributes("-alpha", 0.0)
 
 ROOT = Tk()
 ROOT.overrideredirect(True)
+ROOT.attributes("-topmost", True)
 ROOT.lift()
-ROOT.geometry('200x30+100+100')
+ROOT.attributes("-alpha", 0.0)
+ROOT.geometry('200x20+100+100')
 APP = App(ROOT)
 ROOT.mainloop()
